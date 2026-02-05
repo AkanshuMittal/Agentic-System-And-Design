c7fd6da02da33210168cbff10d599fc006bb0a82
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-05 17:39:48 +0530

    Program to check score is 50 or above using if-else

---
+++ python-logic-assignment/pass_fail_checker.py
@@ -0,0 +1,11 @@
+def pass_fail_checker(score):
+    
+    if score >= 50:
+        print("Pass")
+    else:
+        print("Fail")
+        
+
+score = int(input("Enter the score: "))
+
+pass_fail_checker(score)
