b64666855c0bff4391c9114538a62f1fd2b54b4b
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-07 16:53:33 +0530

    Program to print numbers and skips all even numbers

---
+++ python-loops-assignment/skip_even_numbers.py
@@ -0,0 +1,10 @@
+def skip_even_numbers():
+    
+    for i in  range(1,11):
+        if i%2==0:
+            continue
+        else:
+            print(i)
+            
+skip_even_numbers()
+            
