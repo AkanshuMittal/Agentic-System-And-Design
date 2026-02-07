7bb4be6ddfb72ff013bd7f56f40b137d1fcdef40
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-07 17:27:52 +0530

    Program for adding two numbers and return their sum

---
+++ python-functions-assignment/add_two_numbers.py
@@ -0,0 +1,12 @@
+def add_two_numbers(num1, num2):
+    sum = num1 + num2 
+    
+    return sum
+
+num1= int(input("Enter First number: "))
+num2= int(input("Enter second number: "))
+
+result = add_two_numbers(num1, num2)
+
+print(f"Sum is: {result}")
+
