42c944b96768c710e092f6164adb177a18b3f5f1
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-19 19:46:11 +0530

    Defining function for reading the cleaned numbers from the text file

---
+++ python-files-assignment/file_data_processing_logging.py
@@ -0,0 +1,15 @@
+def read_numbers_from_file(filename):
+    numbers = []
+    
+    with open(filename, "r") as file:
+        print("File opened successfully")
+        
+        for line in file:
+            cleaned_line = line.strip()
+            
+            if cleaned_line:
+                number = int(cleaned_line)
+                numbers.append(number)
+    print(f"Read {len(numbers)} numbers")
+    
+    return numbers 
