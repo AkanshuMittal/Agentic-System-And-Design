a4bf82b2a214b1b34bd113e7cb6749e61b2e0e25
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-19 18:46:37 +0530

    Creating program for process user data and print their details and also admin access

--- python-data-assignment/user_data_processing.py
+++ python-data-assignment/user_data_processing.py
@@ -1,11 +1,10 @@
-def user_details(users):
-    for user in users:
-        scores = user["scores"]
-        average = sum(scores)/len(scores)
+def average(scores):
+    
+    average = sum(scores)/len(scores)
         
-        return average
+    return average
 
-def user_roles(roles):
+def has_admin_access(roles):
     if "admin" in roles:
         return True
     else:
@@ -16,20 +15,23 @@ def main():
     {
         "name": "Abhay",
         "scores": [43, 56, 12, 67],
-        "roles": {"adimn", "editor", "viewer"}
+        "roles": {"editor", "viewer"}
     },
     {
         "name": "Mukul",
         "scores": [13, 96, 42, 47],
-        "roles": {"adimn", "editor", "viewer"}
+        "roles": {"admin", "editor", "viewer"}
     }
 ]
+    for user in users:
+        user_name = user["name"]
+        average_score = average(user["scores"])
+        admin_access = has_admin_access(user["roles"])
         
-    
-    average_score = user_details(users)
-    print(f"Average Score: {average_score}")
-    
-    result = user_roles("roles")
-    print(f"Admin Access: {result}")
+        print(f"User Name: {user_name}")
+        print(f"Average Score: {average_score}")
+        print(f"Admin Access: {'Yes' if admin_access else 'No'}")
+        print()  
+   
     
 main()
