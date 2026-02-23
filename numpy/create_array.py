b614d8f5a852ab5383703ab34e14dd94222993d9
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-23 23:37:52 +0530

    Creating numpy array with existing data, using empty values zeros or ones and also use arange function

---
+++ numpy/create_array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+
+print(arr)
+
+# Creating numpy array with zeroes, ones and using range
+
+zeroes_array = np.zeros(5)
+print(zeroes_array)
+
+ones_array = np.ones(5)
+print(ones_array)
+
+range_array = np.arange(0, 10, 1)
+print(range_array)
+
+
+
