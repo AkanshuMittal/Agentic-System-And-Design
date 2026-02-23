31b85bee17a7906a8f066dd346cb7fa434b37b75
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-23 23:43:09 +0530

    Learn about vectorization in numpy

---
+++ numpy/vectorization.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+arr = np.array([10, 20, 30, 40, 50])
+
+multiply = arr * 4 
+print(multiply)
+
+## Scalar operations 
+
+scores = np.array([80, 90, 70, 85, 95])
+
+print(scores + 5) 
+print(scores * 2)  
+print(scores / 10)
