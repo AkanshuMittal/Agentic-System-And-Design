2fa11fc13605f7826d84b51c6421cc5c8b8ab401
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-24 23:25:47 +0530

    Learn about slicing using 1D and 2D array

---
+++ numpy/slicing.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+## 1D array slicing
+
+a = np.array([1, 2, 3, 4, 5])
+print(a[1:4])
+
+
+## 2D array slicing 
+
+b = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print(b[0:2, 1:3])
+
