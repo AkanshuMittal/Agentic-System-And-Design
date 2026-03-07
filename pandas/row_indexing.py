1d6a1618aceaa59a4c6903676ea0c3e803e232be
AkanshuMittal <akanshumittal150@gmail.com>
2026-03-07 18:27:47 +0530

    Learn about row indexing using loc and iloc

--- pandas/row_indexing.py
+++ pandas/row_indexing.py
@@ -1,12 +1,17 @@
 # Row indexing can be accesssed using either iloc or loc
 
-
 import pandas as pd
 
 df = pd.read_csv("employees.csv")
 print(df.iloc[0:3])
+print()
+
+print(df.loc[0:3])
 
 
 # use loc when u want to access via labels else use iloc
 df.index = ["a", "b", "c", "d", "e", "f", "g", "h"]
 print(df.iloc[0])
+print()
+
+print(df.loc["b"])
