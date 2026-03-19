1139c8d5f1288a8848e208f95108c42c5bce043f
AkanshuMittal <akanshumittal150@gmail.com>
2026-03-19 23:30:38 +0530

    Learning about how to create histogram

---
+++ plotly/histogram.py
@@ -0,0 +1,22 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame({
+    "age": [22, 25, 26, 28, 30, 31, 35, 36, 40, 42]
+})
+
+fig = px.histogram(
+    df,
+    x="age",
+    title='Age distribution of App users'
+)
+
+fig.update_traces(
+    xbins = dict(
+        start = 18,
+        end = 50,
+        size = 8
+    )
+)
+
+fig.show()
