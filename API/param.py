f1c491f3d734babdfe32882bc9735b5e56dbeda5
AkanshuMittal <akanshumittal150@gmail.com>
2026-03-21 23:49:53 +0530

    Learning about parameters which is used in the URL

---
+++ API/param.py
@@ -0,0 +1,18 @@
+import requests
+
+params = {
+    "q" : "agentic",
+    "sort" : "stars",
+    "order" : "desc",
+    "per_page" : 10
+}
+
+response = requests.get("https://api.github.com/search/repositories", params=params)
+
+data = response.json()
+
+if response.status_code == 200:
+    items = data["items"]
+    
+    for i in items:
+        print(i["full_name"])
