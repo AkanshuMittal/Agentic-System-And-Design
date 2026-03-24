d8f604a51b112bf4a69ff3c0db49ecbe77531e85
AkanshuMittal <akanshumittal150@gmail.com>
2026-03-25 01:25:14 +0530

    Creating a scatter plot between Epoch vs Loss

--- matplotlib-visualization-assignment/visualization.py
+++ matplotlib-visualization-assignment/visualization.py
@@ -5,14 +5,23 @@ epochs = np.arange(1,11)
 #print(epochs)
 
 loss = np.linspace(1.0, 0.3, 10)
-print(loss)
+#print(loss)
 
-# Line Plot
+## Line Plot
 plt.figure(figsize=(8,5))
 plt.plot(epochs, loss, marker='o')
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.title("Training Loss vs Epoch")
 plt.grid(True)
+
+plt.show()
+
+## Scatter Plot
+plt.figure(figsize=(8,5))
+plt.scatter(epochs, loss)
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Epoch vs Loss")
 
 plt.show()
